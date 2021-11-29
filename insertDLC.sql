de4409c9e600a3caa33a0e8b51a40fea79fafeca
vinh2vinh <vinh7302000@gmail.com>
2021-11-28 18:49:25 -0800

    ok

---
+++ insertDLC.sql
@@ -0,0 +1,4 @@
+INSERT INTO Charachter
+WHERE char_id in (SELECT char_id 
+                FROM DLC, Character
+                WHERE Character.char_id = DLC.char_id);
--- insertShadow.sql
+++ insertShadow.sql
@@ -1,5 +1,4 @@
-DELETE 
-FROM Charachter
+INSERT INTO Charachter
 WHERE char_id in (SELECT char_id 
                 FROM Shadow_Fighter, Character
                 WHERE Charachter.char_id = Shadow_Fighter.shadow_id)
